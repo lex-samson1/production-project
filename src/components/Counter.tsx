301d0000461b87f8387afe2b41930704d62a4939
Lebedev Alexander <Alexander.Lebedev@okkam.ru>
2023-03-23 21:52:38 +0300

    1.8

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import classes from './Counter.module.scss';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount((prevState) => prevState + 1);
+    };
+
+    return (
+        <div className={classes.button}>
+            <h1>{count}</h1>
+            <button onClick={increment}>increment</button>
+        </div>
+    );
+};
+
+export default Counter;
