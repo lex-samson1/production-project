406e5f94a47c12f3eafb497b7630ed0eac742eeb
lex-samson1 <ip-Alexander.Lebedev@yandex.ru>
2023-10-19 23:42:47 +0300

    fix storybook, add mocks for notifications list

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -1,11 +1,11 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
-import { Theme } from '@/app/providers/ThemeProvider';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/app/providers/ThemeProvider';
 import { ThemeSwitcher } from './ThemeSwitcher';
 
 export default {
-    title: 'shared/ThemeSwitcher',
+    title: 'widgets/ThemeSwitcher',
     component: ThemeSwitcher,
     argTypes: {
         backgroundColor: { control: 'color' },
