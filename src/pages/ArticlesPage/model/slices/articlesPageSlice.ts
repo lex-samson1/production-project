6000585df87525c63fe7dcad403c54fca4d2a800
lex-samson1 <ip-Alexander.Lebedev@yandex.ru>
2023-11-12 14:58:24 +0300

    add toggle flugs and auto remove script, fix sortorder import

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -11,7 +11,7 @@ import {
     ArticleSortField,
 } from '@/entities/Article';
 import { ARTICLES_VIEW_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
-import { SortOrder } from '@/shared/types';
+import { SortOrder } from '@/shared/types/sort';
 import { ArticlesPageSchema } from '../types/articlesPageSchema';
 import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 
