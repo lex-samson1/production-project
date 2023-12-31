36e470a2f4bb4f49087a8b10f2c7fa5e932cee21
lex-samson1 <ip-Alexander.Lebedev@yandex.ru>
2023-12-31 22:23:56 +0300

    redesign profile card

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,6 +69,7 @@ module.exports = {
                     'gap',
                     'feature',
                     'variant',
+                    'size',
                 ],
             },
         ],
