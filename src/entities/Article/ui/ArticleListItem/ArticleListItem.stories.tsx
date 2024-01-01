fa8100f038e9c80c55997fda70ec44f0f346d010
lex-samson1 <ip-Alexander.Lebedev@yandex.ru>
2024-01-01 20:12:40 +0300

    outline btn, skeleton, list view article

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+
 import { ArticleView } from '../../model/consts/articleConsts';
 import { ArticleListItem } from './ArticleListItem';
 import { Article } from '../../model/types/article';
